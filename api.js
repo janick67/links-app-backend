a20b274810c3b7f827a7fa3964abeaa7ac6714cf
janick67 <worwajan67@gmail.com>
2019-09-28 13:41:19 +0200

    Initial commit

---
+++ api.js
@@ -0,0 +1,177 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors')
+const path = require('path');
+const uuid = require('uuid/v4');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const LocalStrategy = require('passport-local').Strategy;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cookieParser());
+
+const data = new Date();
+
+const db = mysql.createConnection({
+  host    : 'localhost',
+  user    : 'janick67',
+  password: 'janick67a',
+  database: 'links'
+})
+
+db.connect((err) => {
+  if(err) throw err;
+  console.log(aktualnaData()+'MySql Connected...');
+});
+
+
+
+const sessionStore = new MySQLStore({
+    expiration: 10800000,
+    createDatabaseTable: true,
+    schema: {
+        tableName: 'sessions',
+        columnNames: {
+            session_id: 'session_id',
+            expires: 'expires',
+            data: 'data'
+}}}, db);
+
+// configure passport.js to use the local strategy
+passport.use(new LocalStrategy(
+  { usernameField: 'email' },
+  (email, password, done) => {
+    console.log('Inside local strategy callback')
+    db.query(`select * from users where email = "${email}"`,(err, result) => {
+        if (err){return console.log(err)};
+        const user = result[0];
+        console.log('Użytkownik z Local: ',email, typeof email,password, typeof password);
+        if (typeof user === 'undefined') {return done(true, false);}
+        console.log('Użytkownik z bazy: ',user.email ,typeof user.email, user.password,typeof user.password);
+        if(email == user.email && password == user.password) {
+          console.log('Local strategy returned true')
+          return done(false, user)
+        }else{
+          console.log('Local strategy returned false')
+          return done(true, false);
+        }
+      });
+    }
+));
+
+// tell passport how to serialize the user
+passport.serializeUser((user, done) => {
+  //console.log('Inside serializeUser callback. User id is save to the session file store here')
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+//  console.log('Inside deserializeUser callback')
+  //console.log(`The user id passport saved in the session file store is: ${id}`)
+  db.query(`select * from users where id = "${id}"`, function (err, rows){
+      //console.log('rows: ', rows);
+        done(err, rows[0]);
+  });
+});
+
+// add & configure middleware
+app.use(session({
+  genid: (req) => {
+    console.log('Inside the session middleware')
+  //  console.log(req.sessionID)
+    return uuid() // use UUIDs for session IDs
+  },
+  store: sessionStore,
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+    if (req.path.indexOf('.css') === -1 && req.path.indexOf('.js') === -1 ){
+        console.log("\n\n\nścieżka: ", req.path);
+        if (typeof req.user !== 'undefined') console.log("użytkownik: ", req.user.username);
+        else console.log("Brak użytkownika");
+        // console.log('-------------------------- session -----------------------------------------');
+        // console.dir(req.sessionID);
+        // console.log('------------------------------------------------------------------------------------');
+        // console.log('-------------------------- cookies -----------------------------------------');
+        // console.dir(req.cookies);
+        // console.log('------------------------------------------------------------------------------------');
+        // console.log('-------------------------- user -----------------------------------------');
+        // console.dir(req.user);
+        // console.log('------------------------------------------------------------------------------------');
+    }
+    next();
+  });
+
+  app.use(function(req, res, next) {
+    if(typeof req.user === 'undefined' && !req.path.indexOf('api/login') >= 0){
+      return res.status(404).send("Najpierw się zaloguj");
+    }
+    next();
+  });
+
+app.post('/signin', (req, res, next) => {
+  console.log('Inside the new POST /login callback')
+  passport.authenticate('local', (err, user, info) => {
+    console.log("(err, user, info)",err, user, info);
+    if (err || !user) return res.send("Nie udało się uwierzytelnic");
+    //console.log('Inside passport.authenticate() callback');
+    //console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+    ///console.log(`req.user: ${JSON.stringify(req.user)}`)
+    req.login(user, (err) => {
+      console.log('Inside req.login() callback')
+      console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+      console.log(`req.user: ${JSON.stringify(req.user)}`);
+      return res.send("Zalogowano pomyślnie");
+    })
+  })(req, res, next)
+});
+
+app.get('/logout',(req, res) => {
+  req.logout();
+  res.send("ok");
+  res.redirect('/');
+});
+
+app.get('/api/links/',(req,res) => {
+    let sql = `SELECT * FROM columns where col_userId = ${req.user.id}`;
+    const query = db.query(sql, (err, result) => {
+    if (err){console.error(err);  return res.send(err)};
+    res.send(result);
+    });
+}
+
+app.use(function(req, res, next) {
+  return res.status(404).send('Route '+req.url+' Not found.');
+});
+
+app.listen(3001, () => console.log(aktualnaData()+'Listen on port 3001....'))
+
+
+
+
+function aktualnaData(){
+  const rok = leadingZero(data.getFullYear());
+  const miesiac = leadingZero(data.getMonth()+1);
+  const dzien = leadingZero(data.getDate());
+  const godz = leadingZero(data.getHours());
+  const min = leadingZero(data.getMinutes());
+  const sec = leadingZero(data.getSeconds());
+  return `[${dzien}.${miesiac}.${rok} ${godz}:${min}:${sec}] `
+}
+
+function leadingZero(i) {
+  return (i < 10)? '0'+i : i;
+}
